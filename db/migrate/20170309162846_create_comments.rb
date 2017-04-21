783b96276960a1f59e75df2e39b808e861803fcd
Terin Carter <terin_carter@mail.ru>
2017-04-21 16:44:29 +0300

    add foreign key

--- db/migrate/20170309162846_create_comments.rb
+++ db/migrate/20170309162846_create_comments.rb
@@ -2,8 +2,14 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.string :text
+      t.integer :user_id
+      t.integer :program_id
 
       t.timestamps null: false
     end
+  add_index :comments, :user_id
+  add_index :comments, :program_id
+  add_foreign_key :comments, :users
+  add_foreign_key :comments, :programs
   end
 end
--- db/migrate/20170309163048_create_users.rb
+++ db/migrate/20170309163048_create_users.rb
@@ -4,8 +4,12 @@ class CreateUsers < ActiveRecord::Migration
       t.string :nickname
       t.string :password
       t.string :email
+      t.integer :role_id
 
       t.timestamps null: false
     end
+  
+  add_index :users, :role_id
+  add_foreign_key :users, :roles
   end
 end
