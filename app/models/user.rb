74613f702a30254f8f7515c2c77eeb4bd8c9a5e0
Terin Carter <terin_carter@mail.ru>
2017-04-23 20:45:01 +0300

    Add encrypted password

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,21 +12,52 @@
 
 class User < ActiveRecord::Base
 
+attr_accessor :password
+
 email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
 validates :nickname, presence: true,
                      :length => { :maximum => 50 },
 		     :uniqueness   => true
 validates :password, presence: true,
- 		     :length => { :maximum => 20 }
+ 		     :length => { :within => 4..40 },
+		     :confirmation => true
+before_save :encrypt_password
 validates :email, presence: true,
 		  :format => { :with => email_regex },
-		  :uniqueness   => true                    
+		  :uniqueness   => true    
+
+def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+end
+
+def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil if user.nil?
+    return user if user.has_password?(submitted_password)
+end
+
+  private
+
+    def encrypt_password
+      self.salt = make_salt if new_record?
+      self.encrypted_password = encrypt(password)
+    end
+
+    def encrypt(string)
+      secure_hash("#{salt}--#{string}")
+    end
+ 
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+ 
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end               
 
 belongs_to :role
 has_many :comments
 has_and_belongs_to_many :programs
-
-
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170421141610) do
+ActiveRecord::Schema.define(version: 20170423173617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -116,8 +116,12 @@ ActiveRecord::Schema.define(version: 20170421141610) do
     t.string   "nickname"
     t.string   "password"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "encrypted_password"
+    t.string   "salt"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
